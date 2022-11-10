fe5f79c3d1ff39a58da4e3ee07d762d979b3f97b
ElliottStratton <elliottanthonystratton@gmail.com>
2022-11-10 13:47:20 -0500

    Added a King class extending piece class

---
+++ src/King.java
@@ -0,0 +1,34 @@
+public class King extends Piece{
+    public King(boolean color, int x, int y, Board b) {
+        super(color, x, y, b);
+        b.occupy(this,x,y);
+    }
+
+    @Override
+    public void move(int x,int y) {
+        if(!check(x,y)) {
+            super.move(x,y);
+        }
+    }
+
+    @Override
+    public boolean isPossible(int x, int y) {
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if(getX() + i == x && getY() == y) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean check(int x, int y) {
+        return false;
+    }
+
+    @Override
+    public int getNum() {
+        return 6;
+    }
+}
