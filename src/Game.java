e2e35a507f37da2c8717e7dde7d967900f83fbaf
calebWarrick <warrickcb20@gcc.edu>
2022-11-15 12:12:08 -0500

    Game class next move function

--- src/Game.java
+++ src/Game.java
@@ -41,6 +41,21 @@ public class Game {
      */
     public void nextMove(int x1, int y1, int x2, int y2)
     {
+        Piece currPiece = board.getPiece(x1,y1);
+        if (currPiece.white != currentPlayer){
+            System.out.println("This is not your piece to move.");
+        }
+        else{
+            if (currPiece.isPossible(x2,y2)){ //isPossible() returns whether that move is possible
+                currPiece.move(x2,y2); //move() throws an IllegalArgumentException with a message for each possible incorrect solution
+            }
+            else{
+                System.out.println("Move is not possible for that piece");
+            }
+        }
+
+
+
         /*
         * Errors: if you try to move a piece that's not yours
         * if you try to move into the same spot
