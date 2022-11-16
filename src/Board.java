0c5ce66f30da4cdafc858057af4f9ce338e4b2d3
calebWarrick <warrickcb20@gcc.edu>
2022-11-15 23:18:20 -0500

    Board constructor uncommented and changed the second half of the pieces to white

--- src/Board.java
+++ src/Board.java
@@ -10,42 +10,42 @@ public class Board {
      */
     public Board() {
         board = new Piece[8][8];
-//        Rook bROne = new Rook(false,0,0,this);
-//        Rook bRTwo = new Rook(false,0,7,this);
-//        Knight bKOne = new Knight(false,0,1,this);
-//        Knight bKTwo = new Knight(false,0,6,this);
-//        Bishop bBOne = new Bishop(false,0,2,this);
-//        Bishop bBTwo = new Bishop(false,0,5,this);
-//        King bKing = new King(false,0,3,this);
-//        Queen bQueen = new Queen(false,0,4,this);
-//
-//        Pawn bPOne = new Pawn(false,1,0,this);
-//        Pawn bPTwo = new Pawn(false,1,1,this);
-//        Pawn bPThree = new Pawn(false,1,2,this);
-//        Pawn bPFour = new Pawn(false,1,3,this);
-//        Pawn bPFive = new Pawn(false,1,4,this);
-//        Pawn bPSix = new Pawn(false,1,5,this);
-//        Pawn bPSeven = new Pawn(false,1,6,this);
-//        Pawn bPEight = new Pawn(false,1,7,this);
-//
-//
-//        Pawn wPOne = new Pawn(false,6,0,this);
-//        Pawn wPTwo = new Pawn(false,6,1,this);
-//        Pawn wPThree = new Pawn(false,6,2,this);
-//        Pawn wPFour = new Pawn(false,6,3,this);
-//        Pawn wPFive = new Pawn(false,6,4,this);
-//        Pawn wPSix = new Pawn(false,6,5,this);
-//        Pawn wPSeven = new Pawn(false,6,6,this);
-//        Pawn wPEight = new Pawn(false,6,7,this);
-//
-//        Rook wROne = new Rook(false,7,0,this);
-//        Rook wRTwo = new Rook(false,7,7,this);
-//        Knight wKOne = new Knight(false,7,1,this);
-//        Knight wKTwo = new Knight(false,7,6,this);
-//        Bishop wBOne = new Bishop(false,7,2,this);
-//        Bishop wBTwo = new Bishop(false,7,5,this);
-//        King wKing = new King(false,7,3,this);
-//        Queen wQueen = new Queen(false,7,4,this);
+        Rook bROne = new Rook(false,0,0,this);
+        Rook bRTwo = new Rook(false,0,7,this);
+        Knight bKOne = new Knight(false,0,1,this);
+        Knight bKTwo = new Knight(false,0,6,this);
+        Bishop bBOne = new Bishop(false,0,2,this);
+        Bishop bBTwo = new Bishop(false,0,5,this);
+        King bKing = new King(false,0,3,this);
+        Queen bQueen = new Queen(false,0,4,this);
+
+        Pawn bPOne = new Pawn(false,1,0,this);
+        Pawn bPTwo = new Pawn(false,1,1,this);
+        Pawn bPThree = new Pawn(false,1,2,this);
+        Pawn bPFour = new Pawn(false,1,3,this);
+        Pawn bPFive = new Pawn(false,1,4,this);
+        Pawn bPSix = new Pawn(false,1,5,this);
+        Pawn bPSeven = new Pawn(false,1,6,this);
+        Pawn bPEight = new Pawn(false,1,7,this);
+
+
+        Pawn wPOne = new Pawn(true,6,0,this);
+        Pawn wPTwo = new Pawn(true,6,1,this);
+        Pawn wPThree = new Pawn(true,6,2,this);
+        Pawn wPFour = new Pawn(true,6,3,this);
+        Pawn wPFive = new Pawn(true,6,4,this);
+        Pawn wPSix = new Pawn(true,6,5,this);
+        Pawn wPSeven = new Pawn(true,6,6,this);
+        Pawn wPEight = new Pawn(true,6,7,this);
+
+        Rook wROne = new Rook(true,7,0,this);
+        Rook wRTwo = new Rook(true,7,7,this);
+        Knight wKOne = new Knight(true,7,1,this);
+        Knight wKTwo = new Knight(true,7,6,this);
+        Bishop wBOne = new Bishop(true,7,2,this);
+        Bishop wBTwo = new Bishop(true,7,5,this);
+        King wKing = new King(true,7,3,this);
+        Queen wQueen = new Queen(true,7,4,this);
     }
 
     /**
