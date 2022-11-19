5892f7b33b453106480ef92582ee5b043a80076f
calebWarrick <warrickcb20@gcc.edu>
2022-11-19 11:35:40 -0500

    Creating an ArrayList of pieces in Board

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * Runs a chess board
  */
 public class Board {
     Piece[][] board;
-
+    ArrayList<Piece> arrPieces = new ArrayList<>(); //Holds all the pieces on the board
 
     /**
      * creates a board with every piece in the correct positions
@@ -11,41 +13,73 @@ public class Board {
     public Board() {
         board = new Piece[8][8];
         Rook bROne = new Rook(false,0,0,this);
+        arrPieces.add(bROne);
         Rook bRTwo = new Rook(false,0,7,this);
+        arrPieces.add(bRTwo);
         Knight bKOne = new Knight(false,0,1,this);
+        arrPieces.add(bKOne);
         Knight bKTwo = new Knight(false,0,6,this);
+        arrPieces.add(bKTwo);
         Bishop bBOne = new Bishop(false,0,2,this);
+        arrPieces.add(bBOne);
         Bishop bBTwo = new Bishop(false,0,5,this);
+        arrPieces.add(bBTwo);
         King bKing = new King(false,0,3,this);
+        arrPieces.add(bKing);
         Queen bQueen = new Queen(false,0,4,this);
+        arrPieces.add(bQueen);
 
         Pawn bPOne = new Pawn(false,1,0,this);
+        arrPieces.add(bPOne);
         Pawn bPTwo = new Pawn(false,1,1,this);
+        arrPieces.add(bPTwo);
         Pawn bPThree = new Pawn(false,1,2,this);
+        arrPieces.add(bPThree);
         Pawn bPFour = new Pawn(false,1,3,this);
+        arrPieces.add(bPFour);
         Pawn bPFive = new Pawn(false,1,4,this);
+        arrPieces.add(bPFive);
         Pawn bPSix = new Pawn(false,1,5,this);
+        arrPieces.add(bPSix);
         Pawn bPSeven = new Pawn(false,1,6,this);
+        arrPieces.add(bPSeven);
         Pawn bPEight = new Pawn(false,1,7,this);
+        arrPieces.add(bPEight);
 
 
         Pawn wPOne = new Pawn(true,6,0,this);
+        arrPieces.add(wPOne);
         Pawn wPTwo = new Pawn(true,6,1,this);
+        arrPieces.add(wPTwo);
         Pawn wPThree = new Pawn(true,6,2,this);
+        arrPieces.add(wPThree);
         Pawn wPFour = new Pawn(true,6,3,this);
+        arrPieces.add(wPFour);
         Pawn wPFive = new Pawn(true,6,4,this);
+        arrPieces.add(wPFive);
         Pawn wPSix = new Pawn(true,6,5,this);
+        arrPieces.add(wPSix);
         Pawn wPSeven = new Pawn(true,6,6,this);
+        arrPieces.add(wPSeven);
         Pawn wPEight = new Pawn(true,6,7,this);
+        arrPieces.add(wPEight);
 
         Rook wROne = new Rook(true,7,0,this);
+        arrPieces.add(wROne);
         Rook wRTwo = new Rook(true,7,7,this);
+        arrPieces.add(wRTwo);
         Knight wKOne = new Knight(true,7,1,this);
+        arrPieces.add(wKOne);
         Knight wKTwo = new Knight(true,7,6,this);
+        arrPieces.add(wKTwo);
         Bishop wBOne = new Bishop(true,7,2,this);
+        arrPieces.add(wBOne);
         Bishop wBTwo = new Bishop(true,7,5,this);
+        arrPieces.add(wBTwo);
         King wKing = new King(true,7,3,this);
+        arrPieces.add(wKing);
         Queen wQueen = new Queen(true,7,4,this);
+        arrPieces.add(wQueen);
     }
 
     /**
