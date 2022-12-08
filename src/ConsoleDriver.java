1f9a94a5bdc8fbdf0b2c04635e13bfeed4615580
ElliottStratton <elliottanthonystratton@gmail.com>
2022-12-08 11:02:19 -0500

    still debugging check/checkmate

--- src/ConsoleDriver.java
+++ src/ConsoleDriver.java
@@ -4,15 +4,15 @@ public class ConsoleDriver {
 
     /**
      * This returns the color of the player from the boolean
+     *
      * @param player is a boolean that determines true for white or false for black
      * @return A string with the color of the player
-     * */
-    public static String getPlayer(boolean player){
+     */
+    public static String getPlayer(boolean player) {
         String sPlayer = "";
-        if(player){
+        if (player) {
             sPlayer = "White";
-        }
-        else{
+        } else {
             sPlayer = "Black";
         }
         return sPlayer;
@@ -20,39 +20,30 @@ public class ConsoleDriver {
 
     /**
      * This takes in a String of one character and makes it an integer
+     *
      * @param x is the character coordinate that the player inputted
      * @return An integer that is the correct coordinate to navigate the 2d array
-     * */
-    public static int convertX(char x)
-    {
+     */
+    public static int convertX(char x) {
         x = Character.toLowerCase(x);
         int numX = 0;
-        if (x == 'a'){
+        if (x == 'a') {
             numX = 0;
-        }
-        else if (x == 'b'){
+        } else if (x == 'b') {
             numX = 1;
-        }
-        else if (x == 'c'){
+        } else if (x == 'c') {
             numX = 2;
-        }
-        else if (x == 'd'){
+        } else if (x == 'd') {
             numX = 3;
-        }
-        else if (x == 'e'){
+        } else if (x == 'e') {
             numX = 4;
-        }
-        else if (x == 'f'){
+        } else if (x == 'f') {
             numX = 5;
-        }
-        else if (x == 'g'){
+        } else if (x == 'g') {
             numX = 6;
-        }
-        else if (x == 'h'){
+        } else if (x == 'h') {
             numX = 7;
-        }
-        else
-        {
+        } else {
             numX = 8;
         }
 //        return x-65;
@@ -61,21 +52,21 @@ public class ConsoleDriver {
 
     /**
      * This takes in an integer of one character and makes it the correct integer
+     *
      * @param y is the integer coordinate that the player inputted
      * @return An integer that is the correct coordinate to navigate the 2d array
-     * */
-    public static int convertY(int y)
-    {
-        return 56-y;
+     */
+    public static int convertY(int y) {
+        return 56 - y;
     }
 
-    public static void run(Game game, Scanner scnr){
+    public static void run(Game game, Scanner scnr) {
         boolean hasWon = false;
-        while(!hasWon){
+        while (!hasWon) {
             String player = getPlayer(game.isCurrentPlayer());
 
             boolean valid = false;
-            while(!valid){
+            while (!valid) {
                 System.out.println(game.getBoard().toString());//For testing purposes
 
                 int x = 0;
@@ -83,7 +74,7 @@ public class ConsoleDriver {
                 boolean hasPiece = false;
                 System.out.println(player + "'s turn to move.");
                 System.out.println("Enter initial piece's coordinates: ");
-                while(!hasPiece) {
+                while (!hasPiece) {
                     String currLoc = scnr.next();
                     x = convertX(currLoc.charAt(0));//For testing purposes
                     //System.out.println("x " + x);
@@ -91,10 +82,9 @@ public class ConsoleDriver {
                     //System.out.println("y " + y);
 
                     //System.out.println(game.getBoard().getPiece(x, y));//For testing purposes
-                    if(x >= 0 && x <= 7 & y >= 0 && y <= 7 && game.getBoard().getPiece(x,y) != null){
+                    if (x >= 0 && x <= 7 & y >= 0 && y <= 7 && game.getBoard().getPiece(x, y) != null) {
                         hasPiece = true;
-                    }
-                    else{
+                    } else {
                         System.out.println("There is not a piece at this location.");
                         System.out.println("Reenter initial piece's coordinates: ");
                     }
@@ -109,21 +99,19 @@ public class ConsoleDriver {
 
                 // System.out.println(game.getBoard().getPiece(newX, newY));//For testing purposes
 
-                try{
-                    game.nextMove(x,y,newX,newY);
+                try {
+                    game.nextMove(x, y, newX, newY);
                     valid = true;
-                }
-                catch(Exception e){
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                     System.out.println("Try reentering the coordinates.");
                 }
             }
             System.out.println(game.getBoard().toString());
-            if (game.checkmate()){
+            if (game.checkmate()) {
                 hasWon = true;
                 System.out.println(player + " has won with a checkmate");
-            }
-            else{
+            } else {
                 game.changePlayer();
             }
         }
@@ -136,37 +124,38 @@ public class ConsoleDriver {
         int choice = 0;
         boolean end = false;
 
+        run(game, scnr);
+
 //        System.out.println("Initial Board");
 //        System.out.println(game.getBoard().toString()); //For testing purposes
 //        System.out.println(Arrays.deepToString(game.getBoard().getBoard())); //For testing purposes
 
-        while(!end) {
-            System.out.println("Enter 1 for single player and 2 for two player");
-            while (true) {
-                int input = scnr.nextInt();
-                if (input == 1) {
-                    choice = 1;
-                    break;
-                } else if (input == 2) {
-                    choice = 2;
-                    break;
-                } else {
-                    System.out.println("Invalid input. Please reenter: ");
-                }
-            }
-            if (choice == 1) {
-                ConsoleAI aiConsole = new ConsoleAI(game);
-                aiConsole.run();
-            } else {
-                run(game, scnr);
-            }
+//        while(!end) {
+//            System.out.println("Enter 1 for single player and 2 for two player");
+//            while (true) {
+//                int input = scnr.nextInt();
+//                if (input == 1) {
+//                    choice = 1;
+//                    break;
+//                } else if (input == 2) {
+//                    choice = 2;
+//                    break;
+//                } else {
+//                    System.out.println("Invalid input. Please reenter: ");
+//                }
+//            }
+//            if (choice == 1) {
+//                ConsoleAI aiConsole = new ConsoleAI(game);
+//                aiConsole.run();
+//            } else {
+//                run(game, scnr);
+//            }
 
-            System.out.println("Enter Go to play again or exit to leave the program:");
-            String exit = scnr.next();
-            if (!exit.equalsIgnoreCase("go")) {
-                end = true;
-            }
+        System.out.println("Enter Go to play again or exit to leave the program:");
+        String exit = scnr.next();
+        if (!exit.equalsIgnoreCase("go")) {
+            end = true;
         }
-    }//End main
+    }
+}//End main
 
-}
